6562ef35171c508723e03f410cdf8735e581bea3
Eric Silva <ericsilvap13@gmail.com>
2025-08-28 13:32:20 -0300

    separalção página recipes individual

--- recipes/urls.py
+++ recipes/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.recipe),
+    path("", views.home),
+    path("recipes/<int:id>/", views.recipe),
 ]
--- recipes/views.py
+++ recipes/views.py
@@ -2,6 +2,10 @@ from django.shortcuts import render
 
 
 # Create your views here.
-def recipe(request):
+def home(request):
     context = {"nome": "Eric Silva"}
     return render(request, "recipes/pages/home.html", context)
+
+
+def recipe(request, id):
+    return render(request, "recipes/pages/recipe-view.html")
