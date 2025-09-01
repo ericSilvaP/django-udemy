ead4a9b20dc6323a9be8940689499463630ef393
Eric Silva <ericsilvap13@gmail.com>
2025-09-01 17:05:12 -0300

    adição de modelos ao admin; população bd

--- recipes/admin.py
+++ recipes/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Recipe, Category
+
 
 # Register your models here.
+class RecipeAdmin(admin.ModelAdmin): ...
+
+
+admin.site.register(Recipe, RecipeAdmin)
+admin.site.register(Category)
--- recipes/models.py
+++ recipes/models.py
@@ -5,6 +5,9 @@ from django.contrib.auth.models import User
 class Category(models.Model):
     name = models.CharField(max_length=65)
 
+    def __str__(self):
+        return self.name
+
 
 # Create your models here.
 class Recipe(models.Model):
@@ -23,3 +26,6 @@ class Recipe(models.Model):
     cover = models.ImageField(upload_to="recipes/covers/%Y/%m/%d/")
     category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True)
     author = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+
+    def __str__(self):
+        return self.title
