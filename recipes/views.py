ae520532e112ab7b73248458cfda6e24488a9c55
Eric Silva <ericsilvap13@gmail.com>
2025-09-02 22:30:14 -0300

    tratamento de erros objetos em categorias

--- recipes/views.py
+++ recipes/views.py
@@ -1,3 +1,4 @@
+from django.http import Http404, HttpResponse
 from django.shortcuts import render
 
 from utils.recipes.factory import make_recipe
@@ -15,9 +16,18 @@ def category(request, category_id):
     recipes = Recipe.objects.filter(
         category__id=category_id, is_published=True
     ).order_by("-id")
+
+    if not recipes:
+        return HttpResponse(content="Not Found", status=404)
+        # raise Http404("Not Found :(")
+
+    category_name = getattr(
+        getattr(recipes.first(), "category", None), "name", "Not Found"
+    )
+
     context = {
         "recipes": recipes,
-        "title": f"{recipes.first().category.name} - Category |",
+        "title": f"{category_name} - Category |",
     }
     return render(request, "recipes/pages/category.html", context)
 
