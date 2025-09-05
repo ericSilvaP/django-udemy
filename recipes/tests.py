27bbce27a960746ede262a798800af2eb72e3efb
Eric Silva <ericsilvap13@gmail.com>
2025-09-05 13:03:02 -0300

    reverse para links dinâmicos

--- recipes/tests.py
+++ recipes/tests.py
@@ -4,5 +4,13 @@ from django.urls import reverse
 
 class RecipeURLsTest(TestCase):
     def test_recipe_home_url_is_ok(self):
-        home_url = reverse("recipes:home")
-        assert home_url == "/"
+        url = reverse("recipes:home")
+        assert url == "/"
+
+    def test_recipe_category_url_is_ok(self):
+        url = reverse("recipes:category", args=(1,))
+        assert url == "/recipes/category/1/"
+
+    def test_recipe_recipe_url_is_ok(self):
+        url = reverse("recipes:recipe", kwargs={"id": 1})
+        assert url == "/recipes/1/"
