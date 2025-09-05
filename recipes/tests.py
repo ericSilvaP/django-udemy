774bc46eb1a375ce40f70b36b6e92fbbe1be2e44
Eric Silva <ericsilvap13@gmail.com>
2025-09-05 13:58:46 -0300

    teste de urls e views com resolve

--- recipes/tests.py
+++ recipes/tests.py
@@ -1,16 +1,32 @@
 from django.test import TestCase
-from django.urls import reverse
+from django.urls import reverse, resolve
+
+from . import views
 
 
 class RecipeURLsTest(TestCase):
-    def test_recipe_home_url_is_ok(self):
+    def test_recipes_home_url_is_ok(self):
         url = reverse("recipes:home")
         assert url == "/"
 
-    def test_recipe_category_url_is_ok(self):
+    def test_recipes_category_url_is_ok(self):
         url = reverse("recipes:category", args=(1,))
         assert url == "/recipes/category/1/"
 
-    def test_recipe_recipe_url_is_ok(self):
+    def test_recipes_recipe_url_is_ok(self):
         url = reverse("recipes:recipe", kwargs={"id": 1})
         assert url == "/recipes/1/"
+
+
+class RecipeViewsTest(TestCase):
+    def test_recipes_home_view_is_ok(self):
+        view = resolve(reverse("recipes:home"))
+        assert view.func is views.home
+
+    def test_recipes_category_view_is_ok(self):
+        view = resolve(reverse("recipes:category", kwargs={"category_id": 1}))
+        assert view.func is views.category
+
+    def test_recipes_recipe_view_is_ok(self):
+        view = resolve(reverse("recipes:recipe", kwargs={"id": 1}))
+        assert view.func is views.recipe
