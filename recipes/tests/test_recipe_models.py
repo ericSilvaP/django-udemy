4f8d4ce9d556f2b5ff7a796255a609537c12a73f
Eric Silva <ericsilvap13@gmail.com>
2025-09-09 09:37:54 -0300

    teste valor default de campos em recupe

--- recipes/tests/test_recipe_models.py
+++ recipes/tests/test_recipe_models.py
@@ -1,12 +1,30 @@
+from os import name
 from django.core.exceptions import ValidationError
 from parameterized import parameterized
-from .test_recipe_base import RecipeTestBase
+from .test_recipe_base import RecipeTestBase, Recipe
 
 
 class RecipeModelTest(RecipeTestBase):
     def setUp(self) -> None:
         self.recipe = self.make_recipe()
         return super().setUp()
+
+    def make_recipe_no_default(self):
+        recipe = Recipe(
+            category=self.make_category(name="Test Default Category"),
+            author=self.make_author(username="newuser"),
+            title="Recipe title",
+            description="Recipe Description",
+            slug="recipe-slug",
+            preparation_time=10,
+            preparation_time_unit="Minutos",
+            servings=5,
+            servings_unit="Porções",
+            preparation_steps="Forma de Preparo da Receita",
+        )
+        recipe.full_clean()
+        recipe.save()
+        return recipe
 
     @parameterized.expand(
         [
@@ -21,3 +39,11 @@ class RecipeModelTest(RecipeTestBase):
 
         with self.assertRaises(ValidationError):
             self.recipe.full_clean()
+
+    def test_recipe_model_preparation_steps_is_html_is_false_by_default(self):
+        recipe = self.make_recipe_no_default()
+        self.assertFalse(recipe.preparation_steps_is_html)
+
+    def test_recipe_model_is_published_is_false_by_default(self):
+        recipe = self.make_recipe_no_default()
+        self.assertFalse(recipe.is_published)
