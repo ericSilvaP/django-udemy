49bd4416dd50cd2346e9e136d28103549aa8c65d
Eric Silva <ericsilvap13@gmail.com>
2025-09-16 10:23:14 -0300

    uso de sessão para salvar dados do formulário; padrão POST/Redirect/GET (PRG)

--- authors/views.py
+++ authors/views.py
@@ -1,10 +1,23 @@
-from django.shortcuts import render
+from django.http import Http404
+from django.shortcuts import redirect, render
 
 from .forms import RegisterForm
 
 
-# Create your views here.
-def home(request):
-    form = RegisterForm()
+def register_view(request):
+    register_form_data = request.session.get("register_form_data")
+    form = RegisterForm(register_form_data)
     context = {"form": form}
+
     return render(request, "authors/pages/register_view.html", context)
+
+
+def register_create(request):
+    if not request.POST:
+        raise Http404
+
+    POST = request.POST
+    request.session["register_form_data"] = POST
+    form = RegisterForm(POST)
+
+    return redirect("authors:register")
