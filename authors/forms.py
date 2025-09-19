3986336b37a2b9bbe8c50250c5f6152d64f3d2a3
Eric Silva <ericsilvap13@gmail.com>
2025-09-18 23:17:41 -0300

    reset geral e adição de campos no forms.py

--- authors/forms.py
+++ authors/forms.py
@@ -1,8 +1,17 @@
-from django.forms import ModelForm
+from django import forms
 from django.contrib.auth.models import User
 
 
-class RegisterForm(ModelForm):
+def add_attr(field, attr_name, attr_new_val):
+    existing = field.widget.attrs.get(attr_name, "")
+    field.widget.attrs[attr_name] = f"{existing} {attr_new_val}".strip()
+
+
+def add_placeholder(field, placeholder_val):
+    add_attr(field, "placeholder", placeholder_val)
+
+
+class RegisterForm(forms.ModelForm):
     class Meta:
         model = User
         fields = [
@@ -12,3 +21,62 @@ class RegisterForm(ModelForm):
             "email",
             "password",
         ]
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        add_placeholder(self.fields["username"], "Your username")
+        add_placeholder(self.fields["email"], "Your e-mail")
+        add_placeholder(self.fields["first_name"], "Ex.: John")
+        add_placeholder(self.fields["last_name"], "Ex.: Doe")
+        add_attr(self.fields["username"], "css", "a-css-class")
+
+    password = forms.CharField(
+        error_messages={"required": "Password can't be empty"},
+        help_text="Password must have at least: one uppercase letter, one lowercase letter and one number. Lenght: minimum 8 characters",
+        widget=forms.PasswordInput(attrs={"placeholder": "Enter your password"}),
+    )
+
+    password_repeat = forms.CharField(
+        widget=forms.PasswordInput(attrs={"placeholder": "Repeat your Password"})
+    )
+from django import forms
+from django.contrib.auth.models import User
+
+
+def add_attr(field, attr_name, attr_new_val):
+    existing = field.widget.attrs.get(attr_name, "")
+    field.widget.attrs[attr_name] = f"{existing} {attr_new_val}".strip()
+
+
+def add_placeholder(field, placeholder_val):
+    add_attr(field, "placeholder", placeholder_val)
+
+
+class RegisterForm(forms.ModelForm):
+    class Meta:
+        model = User
+        fields = [
+            "first_name",
+            "last_name",
+            "username",
+            "email",
+            "password",
+        ]
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        add_placeholder(self.fields["username"], "Your username")
+        add_placeholder(self.fields["email"], "Your e-mail")
+        add_placeholder(self.fields["first_name"], "Ex.: John")
+        add_placeholder(self.fields["last_name"], "Ex.: Doe")
+        add_attr(self.fields["username"], "css", "a-css-class")
+
+    password = forms.CharField(
+        error_messages={"required": "Password can't be empty"},
+        help_text="Password must have at least: one uppercase letter, one lowercase letter and one number. Lenght: minimum 8 characters",
+        widget=forms.PasswordInput(attrs={"placeholder": "Enter your password"}),
+    )
+
+    password_repeat = forms.CharField(
+        widget=forms.PasswordInput(attrs={"placeholder": "Repeat your Password"})
+    )
