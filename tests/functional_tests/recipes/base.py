1ed5e9575bca048b5274088ece60620bfd5578d1
Eric Silva <ericsilvap13@gmail.com>
2025-09-29 22:56:08 -0300

    recipe mixin e patch nos testes funcionais de recipes

--- tests/functional_tests/recipes/base.py
+++ tests/functional_tests/recipes/base.py
@@ -1,8 +1,9 @@
+from recipes.tests.test_recipe_base import RecipeMixin
 from utils.browser import make_brave_browser
 from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 
 
-class RecipeBaseFunctionalTest(StaticLiveServerTestCase):
+class RecipeBaseFunctionalTest(StaticLiveServerTestCase, RecipeMixin):
     def setUp(self) -> None:
         self.browser = make_brave_browser()
         return super().setUp()
--- tests/functional_tests/recipes/test_recipe_home_page.py
+++ tests/functional_tests/recipes/test_recipe_home_page.py
@@ -1,11 +1,17 @@
+import time
+from unittest.mock import patch
 from selenium.webdriver.common.by import By
+
+from recipes.tests.test_recipe_base import RecipeMixin
 from .base import RecipeBaseFunctionalTest
 import pytest
 
 
 @pytest.mark.functional_test
 class RecipesHomePageTest(RecipeBaseFunctionalTest):
+    @patch("recipes.views.PER_PAGE", new=3)
     def test_the_test(self):
+        self.make_recipes_in_batch(5)
         self.browser.get(self.live_server_url)
         body = self.browser.find_element(By.TAG_NAME, "body").text
         self.assertIn("Não há receitas", body)
