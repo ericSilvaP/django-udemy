efbe6df0840b3fa390dfa24a0c5bde49515c24a3
Eric Silva <ericsilvap13@gmail.com>
2025-09-30 22:41:03 -0300

    teste funcional pesquisa de recipe por titulo

--- tests/functional_tests/recipes/test_recipe_home_page.py
+++ tests/functional_tests/recipes/test_recipe_home_page.py
@@ -1,17 +1,43 @@
 import time
 from unittest.mock import patch
 from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+import os
+from dotenv import load_dotenv
 
-from recipes.tests.test_recipe_base import RecipeMixin
 from .base import RecipeBaseFunctionalTest
 import pytest
+
+load_dotenv()
 
 
 @pytest.mark.functional_test
 class RecipesHomePageTest(RecipeBaseFunctionalTest):
-    @patch("recipes.views.PER_PAGE", new=3)
-    def test_the_test(self):
-        self.make_recipes_in_batch(5)
+    def test_recipes_home_error_message_without_recipes(self):
         self.browser.get(self.live_server_url)
         body = self.browser.find_element(By.TAG_NAME, "body").text
         self.assertIn("Não há receitas", body)
+
+    def test_search_input_can_find_correct_recipe(self):
+        recipes = self.make_recipes_in_batch(5)
+
+        needed_title = "Recipe Title Test"
+        recipes[0].title = needed_title
+        recipes[0].save()
+
+        # user open the browser
+        self.browser.get(self.live_server_url)
+
+        # click in search bar
+        search_input = self.browser.find_element(
+            By.CSS_SELECTOR,
+            ".search-input",
+        )
+
+        # search for recipe
+        search_input.send_keys(needed_title)
+        search_input.send_keys(Keys.ENTER)
+
+        # search recult must appear correctly
+        body = self.browser.find_element(By.TAG_NAME, "body").text
+        self.assertIn(needed_title, body)
