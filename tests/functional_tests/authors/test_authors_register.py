45453f024b1c3f3fc853273c20d6f92df20b44f1
Eric Silva <ericsilvap13@gmail.com>
2025-10-01 10:07:49 -0300

    testes parametrizados mensagens de erro campos com espaços register de authors

--- tests/functional_tests/authors/test_authors_register.py
+++ tests/functional_tests/authors/test_authors_register.py
@@ -1,6 +1,41 @@
 from .base import AuthorsBaseTest
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from parameterized import parameterized
 
 
 class AuthorsRegisterTest(AuthorsBaseTest):
-    def test_the_test(self):
-        self.browser.get(self.live_server_url)
+    def fill_form_dummy_data(self, form):
+        fields = form.find_elements(By.TAG_NAME, "input")
+
+        for field in fields:
+            if field.is_displayed():
+                field.send_keys(" " * 20)
+
+    @parameterized.expand(
+        [
+            "Write your first name",
+            "Write your last name",
+            "Write your username",
+            "Password must not be empty",
+            "Please, repeat your password",
+        ]
+    )
+    def test_fields_with_spaces_error_messages(self, error_message):
+        self.browser.get(self.live_server_url + "/authors/register/")
+
+        form = self.browser.find_element(
+            By.CSS_SELECTOR, ".register-form-container form"
+        )
+
+        self.fill_form_dummy_data(form)
+
+        email_field = form.find_element(By.NAME, "email")
+        email_field.send_keys("dummy@email.com")
+        email_field.send_keys(Keys.ENTER)
+
+        form = self.browser.find_element(
+            By.CSS_SELECTOR, ".register-form-container form"
+        )
+
+        self.assertIn(error_message, form.text)
