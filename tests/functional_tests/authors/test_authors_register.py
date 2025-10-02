dc9b6806a4ce43e2bec48a0ecca818f3bec10cd6
Eric Silva <ericsilvap13@gmail.com>
2025-10-02 17:59:14 -0300

    testes funcionais campos de formulario em alguns cenarios

--- tests/functional_tests/authors/test_authors_register.py
+++ tests/functional_tests/authors/test_authors_register.py
@@ -5,6 +5,11 @@ from parameterized import parameterized
 
 
 class AuthorsRegisterTest(AuthorsBaseTest):
+    def get_form(self):
+        return self.browser.find_element(
+            By.CSS_SELECTOR, ".register-form-container form"
+        )
+
     def fill_form_dummy_data(self, form):
         fields = form.find_elements(By.TAG_NAME, "input")
 
@@ -24,9 +29,7 @@ class AuthorsRegisterTest(AuthorsBaseTest):
     def test_fields_with_spaces_error_messages(self, error_message):
         self.browser.get(self.live_server_url + "/authors/register/")
 
-        form = self.browser.find_element(
-            By.CSS_SELECTOR, ".register-form-container form"
-        )
+        form = self.get_form()
 
         self.fill_form_dummy_data(form)
 
@@ -34,8 +37,63 @@ class AuthorsRegisterTest(AuthorsBaseTest):
         email_field.send_keys("dummy@email.com")
         email_field.send_keys(Keys.ENTER)
 
-        form = self.browser.find_element(
-            By.CSS_SELECTOR, ".register-form-container form"
-        )
+        form = self.get_form()
 
         self.assertIn(error_message, form.text)
+
+    def test_invalid_email_error_message(self):
+        self.browser.get(self.live_server_url + "/authors/register/")
+
+        form = self.get_form()
+        self.fill_form_dummy_data(form)
+        email_field = form.find_element(By.NAME, "email")
+        email_field.clear()
+        email_field.send_keys("dummy@email")
+        email_field.send_keys(Keys.ENTER)
+
+        # form reselect after page refresh
+        form = self.get_form()
+
+        self.assertIn("Informe um endereço de email válido.", form.text)
+
+    def test_passwords_doesnt_match_error_message(self):
+        self.browser.get(self.live_server_url + "/authors/register/")
+
+        form = self.get_form()
+
+        self.fill_form_dummy_data(form)
+
+        password_field = form.find_element(By.NAME, "password")
+        password_repeat_field = form.find_element(By.NAME, "password_repeat")
+
+        password_field.send_keys("password1")
+        password_repeat_field.send_keys("password2")
+        password_repeat_field.send_keys(Keys.ENTER)
+
+        form = self.get_form()
+        self.assertIn("Passwords must be equals", form.text)
+
+    def test_register_form_correctly_filled_message(self):
+        self.browser.get(self.live_server_url + "/authors/register/")
+
+        form = self.get_form()
+
+        first_name = form.find_element(By.NAME, "first_name")
+        last_name = form.find_element(By.NAME, "last_name")
+        username = form.find_element(By.NAME, "username")
+        email = form.find_element(By.NAME, "email")
+        password = form.find_element(By.NAME, "password")
+        password_repeat = form.find_element(By.NAME, "password_repeat")
+
+        first_name.send_keys("first")
+        last_name.send_keys("last")
+        username.send_keys("user")
+        email.send_keys("abc@email.com")
+        password.send_keys("abcd1234")
+        password_repeat.send_keys("abcd1234")
+        password_repeat.send_keys(Keys.ENTER)
+
+        self.assertIn(
+            "Usuário cadastrado com sucesso",
+            self.browser.find_element(By.TAG_NAME, "body").text,
+        )
