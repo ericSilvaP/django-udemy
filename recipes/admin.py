8c30b3148a3f03ac1bc338a256f6b9c8ab010776
Eric Silva <ericsilvap13@gmail.com>
2025-10-03 23:34:24 -0300

    configuração RecipeAdmin

--- recipes/admin.py
+++ recipes/admin.py
@@ -2,9 +2,17 @@ from django.contrib import admin
 from .models import Recipe, Category
 
 
-# Register your models here.
-class RecipeAdmin(admin.ModelAdmin): ...
+class CategoryAdmin(admin.ModelAdmin): ...
+
+
+class RecipeAdmin(admin.ModelAdmin):
+    list_display = ["id", "title", "author", "is_published"]
+    list_display_links = ["title", "author"]
+    list_editable = ["is_published"]
+    list_filter = ["author", "is_published"]
+    ordering = ["-id"]
+    list_per_page = 10
 
 
 admin.site.register(Recipe, RecipeAdmin)
-admin.site.register(Category)
+admin.site.register(Category, CategoryAdmin)
