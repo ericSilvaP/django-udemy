ea2b34e919ca76f1b22893447fafbd898900a33b
Eric Silva <ericsilvap13@gmail.com>
2025-10-07 23:59:50 -0300

    edição receita campos imagem, tempo e porções estilizados com widgets e classes

--- authors/forms/recipe_form.py
+++ authors/forms/recipe_form.py
@@ -21,3 +21,20 @@ class AuthorsRecipeForm(forms.ModelForm):
         widget=forms.Textarea(attrs={"class": "span-2"})
     )
     cover = forms.ImageField(widget=forms.FileInput(attrs={"class": "span-2"}))
+    servings_unit = forms.CharField(
+        widget=forms.Select(
+            choices=(
+                ("porções", "Porções"),
+                ("fatias", "Fatias"),
+                ("pessoas", "Pessoas"),
+            )
+        )
+    )
+    preparation_time_unit = forms.CharField(
+        widget=forms.Select(
+            choices=(
+                ("minutos", "Minutos"),
+                ("horas", "Horas"),
+            )
+        )
+    )
