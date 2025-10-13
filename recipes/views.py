359ad4c84c36ad53865254a4ed83b65223e1b3e0
Eric Silva <ericsilvap13@gmail.com>
2025-10-13 17:10:37 -0300

    classe base para exibição de multiplas receitas[

--- recipes/views.py
+++ recipes/views.py
@@ -1,7 +1,7 @@
 from django.http import Http404
 from django.shortcuts import get_list_or_404, get_object_or_404, render
 from django.db.models import Q
-
+from django.views.generic import ListView
 from utils.pagination import make_pagination
 
 from .models import Recipe
@@ -10,7 +10,31 @@ import os
 PER_PAGE = os.environ.get("PER_PAGE", 6)
 
 
-# Create your views here.
+class RecipeListViewBase(ListView):
+    model = Recipe
+    context_object_name = "recipes"
+    ordering = "-id"
+    template_name = "recipes/pages/home.html"
+
+    def get_queryset(self, *args, **kwargs):
+        queryset = super().get_queryset(*args, **kwargs)
+        queryset = queryset.filter(is_published=True)
+        return queryset
+
+    def get_context_data(self, *args, **kwargs):
+        context = super().get_context_data(*args, **kwargs)
+        page_obj, pagination_range = make_pagination(
+            self.request, context.get("recipes"), PER_PAGE
+        )
+        context.update(
+            {
+                "recipes": page_obj,
+                "pagination_range": pagination_range,
+            }
+        )
+        return context
+
+
 def home(request):
     recipes = Recipe.objects.filter(is_published=True).order_by("-id")
 
