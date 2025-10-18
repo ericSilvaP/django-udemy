ef9adb8a72c9f1c26f98ea5748982b912f2a75d6
Eric Silva <ericsilvap13@gmail.com>
2025-10-18 15:20:48 -0300

    allowed hosts permite todos

--- project/settings.py
+++ project/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY", "INSECURE")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = int(os.environ.get("DEBUG", 1))
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
